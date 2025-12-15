9418966e52b34bb843d31c9048f444279342ea70
alchemistsGestalt <zeinhatem@proton.me>
2025-12-15 12:21:46 +0200

    Streamlined old code to be easier to edit

--- src/priority.py
+++ src/priority.py
@@ -3,26 +3,27 @@
 DEFAULT_DAY_END = '16:00'
 
 def add_study_tasks(schedule: list[list[dict]]) -> list:
-    newdays = [[] for _ in range(7)]
-    days = ([task for task in day
-             if task['taskName'].split()[-1] != 'Study']
-            or [{'end': DEFAULT_DAY_END}]
-            for day in schedule)
-    day_endings = [
-        sorted(day, key=lambda t: t['end'])[-1]['end'].split(':')
-        for day in days
+    new_schedule = [[] for _ in range(7)]
+    old_tasks = [[task for task in day if task['isGenerated'] == False]
+                 for day in schedule]
+    end_times = [
+        sorted(day, key=lambda t: t['taskEnd'])[-1]['taskEnd'].split(':')
+        if day else DEFAULT_DAY_END.split(':')
+        for day in old_tasks
     ]
-    for i, day in enumerate(schedule):
+    for i, day in enumerate(old_tasks):
         for task in day:
-            subject = ' '.join(task['taskName'].split()[:-1])
-            type = task['taskName'].split()[-1].lower()
-            if type == 'lecture':
-                tomorrow = (i + 1) % 7
-                hour, minute = [int(x) for x in day_endings[tomorrow]]
-                newdays[(i + 1) % 7].append({'taskName': f'{subject} Study',
-                                            'start': f'{hour+2:02}:{minute:02}',
-                                            'end': f'{hour+4:02}:{minute:02}',
-                                            'priority': 'low'})
-                day_endings[tomorrow] = (hour+2, minute)
-            newdays[i].append(task)
-    return newdays
+            new_schedule[i].append(task)
+            if task['taskType'].lower() != 'lecture':
+                continue
+            study_task = {}
+            tomorrow = (i + 1) % 7
+            hour, minute = (int(x) for x in end_times[tomorrow])
+            study_task['taskName'] = task['taskName']
+            study_task['taskStart'] = f'{hour+2:02}:{minute:02}'
+            study_task['taskEnd'] = f'{hour+4:02}:{minute:02}'
+            study_task['taskPriority'] = task['taskPriority']
+            study_task['taskDesc'] = f"Study content of yesterday's {task['taskName']} lecture."
+            new_schedule[tomorrow].append(study_task)
+            end_times[tomorrow] = (hour + 2, minute)
+    return new_schedule
