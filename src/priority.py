410cf913f2ef7509df32c72e4fb75681196cfb8b
alchemistsGestalt <zeinhatem@proton.me>
2025-12-18 15:14:13 +0200

    feat: use taskPriority for order and duration
    Study tasks are ordered by priority and high priority tasks are 2 hours
    long instead of 1 hour.

--- src/priority.py
+++ src/priority.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 DEFAULT_DAY_END = '16:00'
+Schedule = list[list[dict]]
 
-def add_study_tasks(schedule: list[list[dict]]) -> list:
+def add_study_tasks(schedule: Schedule) -> Schedule:
     new_schedule = [[] for _ in range(7)]
-    old_tasks = [[task for task in day if task['isGenerated'] == False]
+    old_tasks = [[task for task in day if not task.get('isGenerated', True)]
                  for day in schedule]
     end_times = [
         sorted(day, key=lambda t: t['taskEnd'])[-1]['taskEnd'].split(':')
         if day else DEFAULT_DAY_END.split(':')
         for day in old_tasks
     ]
+    studies = [[] for _ in range(7)]
     for i, day in enumerate(old_tasks):
         for task in day:
             new_schedule[i].append(task)
@@ -18,12 +20,30 @@ def add_study_tasks(schedule: list[list[dict]]) -> list:
                 continue
             study_task = {}
             tomorrow = (i + 1) % 7
-            hour, minute = (int(x) for x in end_times[tomorrow])
             study_task['taskName'] = task['taskName']
-            study_task['taskStart'] = f'{hour+2:02}:{minute:02}'
-            study_task['taskEnd'] = f'{hour+4:02}:{minute:02}'
+            study_task['taskType'] = 'study'
             study_task['taskPriority'] = task['taskPriority']
             study_task['taskDesc'] = f"Study content of yesterday's {task['taskName']} lecture."
-            new_schedule[tomorrow].append(study_task)
-            end_times[tomorrow] = (hour + 2, minute)
+            study_task['isGenerated'] = True
+            studies[tomorrow].append(study_task)
+    schedule_tasks(studies, end_times, schedule=new_schedule)
     return new_schedule
+
+def schedule_tasks(tasks: Schedule, start,
+                   *, schedule: Schedule | None = None):
+    schedule = schedule or [[] for _ in range(7)]
+    for i, day in enumerate(tasks):
+        if not day:
+            continue
+        tasks_by_priority = sorted(
+            day, key=lambda d: \
+                {'high': 1, 'medium': 2, 'low': 3}[d.get('taskPriority', 'low')]
+        )
+        for task in tasks_by_priority:
+            hour, minute = (int(x) for x in start[i])
+            duration = 2 if task['taskPriority'] == 'high' else 1
+            task['taskStart'] = f'{hour+2:02}:{minute:02}'
+            task['taskEnd'] = f'{hour+2+duration:02}:{minute:02}'
+            start[i] = (hour + duration, minute)
+            schedule[i].append(task)
+    return schedule
